9a27fcde6366a68007b9fb061acaa8988df08d53
hunter <hunter0409>
2022-02-17 20:59:45 +0300

    attact boss contract create

--- scripts/run.js
+++ scripts/run.js
@@ -6,18 +6,28 @@ const main = async () => {
             "https://ibb.co/PgHrmRy",
             "https://ibb.co/3SQHFDW"],
         [100, 200, 300],                    // HP values
-        [100, 50, 25]                       // Attack damage values
+        [100, 50, 25],
+        "Elon Musk", // Boss name
+        "https://ibb.co/3SQHFDW", // Boss image
+        10000, // Boss hp
+        50 // Boss attack damage
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 
     let txn;
 
-    txn = await gameContract.mintCharacterNFT(2);
+    txn = await gameContract.mintCharacterNFT(1);
     await txn.wait();
 
-    let returnedTokenUri = await gameContract.tokenURI(1);
-    console.log("TokenUri:", returnedTokenUri);
+    // let returnedTokenUri = await gameContract.tokenURI(1);
+    // console.log("TokenUri:", returnedTokenUri);
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
 };
 
 // https://ibb.co/3B0kFD8
