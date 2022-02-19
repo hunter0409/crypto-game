0c6df10117f698087c945b2a85089a2cc8818155
hunter <hunter0409>
2022-02-19 09:57:15 -0500

    mint images in IPFS

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -49,13 +49,13 @@ const main = async () => {
 
     const gameContract = await gameContractFactory.deploy(
         ["Leo", "Aang", "Pikachu"],       // Names
-        ["https://ibb.co/3B0kFD8", // Images
-            "https://ibb.co/PgHrmRy",
-            "https://ibb.co/3SQHFDW"],
+        ["https://gateway.pinata.cloud/ipfs/QmZLiiPyzZ9V2metcCBWHapNWiWD8U5PvEkVi2LTUDk7Ly/1.png", // Images
+            "https://gateway.pinata.cloud/ipfs/QmZLiiPyzZ9V2metcCBWHapNWiWD8U5PvEkVi2LTUDk7Ly/10.png",
+            "https://gateway.pinata.cloud/ipfs/QmZLiiPyzZ9V2metcCBWHapNWiWD8U5PvEkVi2LTUDk7Ly/2.png"],
         [100, 200, 300],                    // HP values
         [100, 50, 25],
         "Elon Musk", // Boss name
-        "https://ibb.co/3SQHFDW", // Boss image
+        "https://gateway.pinata.cloud/ipfs/QmZLiiPyzZ9V2metcCBWHapNWiWD8U5PvEkVi2LTUDk7Ly/3.png", // Boss image
         10000, // Boss hp
         50 // Boss attack damage
     );
